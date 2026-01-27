24f15971fe1d1a1986be048e53bf47fe170b6bdb
Samuel Bassaly <shkshk90@gmail.com>
2026-01-27 15:19:39 +0100

    Move to sycl

--- docker/run.cmake.sh
+++ docker/run.cmake.sh
@@ -7,18 +7,6 @@ if [ ! -d "/oneMKLwithCublas/lib" ]; then
     /tmp/install_mkl.sh
 fi
 
-cmake --build /build/cuda -j 12
-
-mkdir -p /install/new/cuda
-
-
-cmake --install /build/cuda --prefix /install/new/cuda
-
-echo "run ::    export LD_LIBRARY_PATH=/install/new/cuda/lib:\$LD_LIBRARY_PATH "
-echo "          /install/new/cuda/bin/test_all"
-
-exit 0
-
 cmake --build /build/icpx -j 12
 mkdir -p /install/new/icpx
 cmake --install /build/icpx --prefix /install/new/icpx
